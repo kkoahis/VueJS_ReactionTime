2f1517eb1ad3d3bbce8220e24efa39720587e411
kkoahis <khoakun27@gmail.com>
2023-12-07 23:04:55 +0700

    Lifecycle hooks, Creating a timer(set interval), Custom events with data

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Test Reaction Time</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Result :score="score" v-if="showResult" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue';
+import Result from './components/Result.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Block, Result
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResult: false
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 1000 + Math.floor(Math.random() * 5000);
+      this.isPlaying = true;
+      this.showResult = false;
+      // console.log(this.delay)
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResult = true;
+    }
   }
 }
 </script>
@@ -20,7 +45,24 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-style: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+
+button:disabled {
+  background: #999;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,42 @@
+<template>
+    <h1 class="score">Your reaction: {{ score }}ms</h1>
+    <p class="rank">Your are {{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = "go pro bro!"
+        } else if (this.score < 500) {
+            this.rank = "normal"
+        } else {
+            this.rank = "chicken"
+        }
+    }
+}
+</script>
+
+<style>
+.score {
+    text-align: center;
+    width: 350px;
+    margin: 20px auto;
+    padding: 10px;
+    border-radius: 5px;
+    background: #12d0a1;
+    color: white;
+}
+
+.rank {
+    font-style: 1.4em;
+    color: crimson;
+    font-weight: bold;
+}
+</style>
